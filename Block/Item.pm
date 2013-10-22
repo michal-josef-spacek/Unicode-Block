9b171e2fdade159063b3c1608790b1a8b9baf90a
Michal Špaček <tupinek@gmail.com>
2013-10-23 01:38:50 +0200

    Add doc for Unicode::Block::Item.

--- Block/Item.pm
+++ Block/Item.pm
@@ -115,3 +115,133 @@ sub _is_hex {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Unicode::Block::Item - Class for unicode block character.
+
+=head1 SYNOPSIS
+
+ use Unicode::Block::Item;
+ my $obj = Unicode::Block::Item->new(%parameters);
+ my $base = $obj->base;
+ my $char = $obj->char;
+ my $hex = $obj->hex;
+ my $last_hex = $obj->last_hex;
+
+=head1 METHODS
+
+=over 8
+
+=item C<new(%parameters)>
+
+Constructor.
+
+=over 8
+
+=item * C<hex>
+
+ Hexadecimal number.
+ Default value is undef.
+ It is required.
+
+=item * C<hex_length>
+
+ Length of hex number.
+ Default value is 4.
+
+=back
+
+=item C<base()>
+
+ Get hex base number in format 'U+???x'.
+ Example: 'hex' => 1234h; Returns 'U+123x'.
+ Returns string with hex base number.
+
+=item C<char()>
+
+ Get character.
+ Example: 'hex' => 1234h; Returns 'ሴ'.
+ Returns string with character.
+
+=item C<hex()>
+
+ Get hex number in 'hex_length' length.
+ Example: 'hex' => 1234h; Returns '0x1234'.
+ Returns string with hex number.
+
+=item C<last_hex()>
+
+ Get last hex number.
+ Example: 'hex' => 1234h; Returns '4'.
+ Returns string with last hex number.
+
+=back
+
+=head1 ERRORS
+
+ new():
+         Parameter 'hex' is required.
+         Parameter 'hex' isn't hexadecimal number.
+         From Class::Utils::set_params():
+                 Unknown parameter '%s'.
+
+=head1 EXAMPLE
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Unicode::Block::Item;
+
+ # Object.
+ my $obj = Unicode::Block::Item->new(
+        'hex' => 2505,
+ );
+
+ # Print out.
+ print 'Character: '.encode_utf8($obj->char)."\n";
+ print 'Hex: '.$obj->hex."\n";
+ print 'Last hex character: '.$obj->last_hex."\n";
+ print 'Base: '.$obj->base."\n";
+
+ # Output.
+ # Character: ┅
+ # Hex: 2505
+ # Last hex character: 5
+ # Base: U+250x
+
+=head1 DEPENDENCIES
+
+L<Class::Utils>,
+L<Error::Pure>,
+L<Readonly>,
+L<Unicode::Char>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Unicode-Block>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+BSD license.
+
+=head1 VERSION
+
+0.01
+
+=cut
