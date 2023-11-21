d0415a3e254a86686be0cea47b90d33b12fd0078
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-11-21 14:00:33 +0100

    Improve LICENSE AND COPYRIGHT section in doc

--- Block/Item.pm
+++ Block/Item.pm
@@ -266,8 +266,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2013-2017 Michal Josef Špaček
- BSD 2-Clause License
+© 2013-2017 Michal Josef Špaček
+
+BSD 2-Clause License
 
 =head1 VERSION
 
